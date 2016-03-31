8096aa3303ce30936abc2c260c8438f1fc6b5d23
mashimo_k40 <mashimo@k40.fun.bio.keio.ac.jp>
2016-03-31 20:35:27 +0900

    まじでK40で動くようにした

--- outputImage.cpp
+++ outputImage.cpp
@@ -12,10 +12,11 @@
 #include "searchFunction.h"
 using namespace std;
 //opencv 
-#include <cv.h>
-#include <highgui.h>
-//#include <opencv2/opencv.hpp>
+//#include <cv.h>
+//#include <highgui.h>
+#include <opencv2/opencv.hpp> //K40
 //#include <opencv2/highgui.hpp>
+#include <opencv2/highgui/highgui.hpp>//K40
 #define barSizeX 20
 #define barSizeY 400
 #define cbAreaX 100
